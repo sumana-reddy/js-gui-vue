41607542ab85d494a8f40192dfb99e72a6528d6f
sumanareddy <sumanareddy.guc27@gmail.com>
2020-02-28 10:44:39 -0600

    first commit

---
+++ components/multiplier.js
@@ -0,0 +1,18 @@
+const multiply = (x,y,z) => {return x * y * z }
+
+const multiplier = new Vue({
+    el: '#multiplier',
+    data: {
+      firstnumber: 4,
+      secondnumber: 3
+      thirdnumber: 5,
+    },
+    computed: {
+        multiplier : function () {
+          const i = parseInt(this.firstnumber)
+          const j = parseInt(this.secondnumber)
+          const k = parseInt(this.thirdnumber)
+          return `The multiplication is ${multiply( i, j , k )}.`
+        }
+      }
+    })
